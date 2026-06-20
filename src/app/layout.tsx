22190bffac9e70173515fa3bcaaefc5f3afa08f1
L.S.C.F <139922900+astur84@users.noreply.github.com>
2026-06-20 15:17:31 +0200

    Update body class for layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
+import './globals.css'; 
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
-      {/* Añade esta clase bg-red-500 temporalmente */}
-      <body className="bg-red-500">
+      <body className="bg-bg text-text"> {/* Añadimos la clase aquí para probar */}
         {children}
       </body>
     </html>
